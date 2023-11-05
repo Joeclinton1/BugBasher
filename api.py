502c514a9147035e60000b46f8196e73c0663bd3
Joeclinton1 <joeclinton1@btinternet.com>
2023-11-05 18:12:01 +0000

    Remove extra api files

--- api.py
+++ api.py
@@ -1,25 +1,43 @@
+import html
+import json
+
 import requests
-import sys
 
 # For local streaming, the websockets are hosted without ssl - http://
 HOST = 'localhost:5000'
-URI = f'http://{HOST}/api/v1/generate'
+URI = f'http://{HOST}/api/v1/chat'
 
 # For reverse-proxied streaming, the remote will likely host with ssl - https://
-# URI = 'https://your-uri-here.trycloudflare.com/api/v1/generate'
+# URI = 'https://your-uri-here.trycloudflare.com/api/v1/chat'
 
-async def print_response_stream(prompt):
-    async for response in run(prompt):
-        print(response, end='')
-        sys.stdout.flush()  # If we don't flush, we won't see tokens in realtime.
 
-def run(prompt):
+def run(user_input):
+    history = {'internal': [], 'visible': []}
     request = {
-        'prompt': prompt,
-        'max_new_tokens': 1500,
+        'user_input': user_input,
+        'max_new_tokens': 250,
         'auto_max_new_tokens': False,
         'max_tokens_second': 0,
-        'preset': 'simple-1',
+        'history': history,
+        'mode': 'instruct',  # Valid options: 'chat', 'chat-instruct', 'instruct'
+        'character': 'Example',
+        'instruction_template': 'Alpaca',  # Will get autodetected if unset
+        'your_name': 'You',
+        # 'name1': 'name of user', # Optional
+        # 'name2': 'name of character', # Optional
+        # 'context': 'character context', # Optional
+        # 'greeting': 'greeting', # Optional
+        # 'name1_instruct': 'You', # Optional
+        # 'name2_instruct': 'Assistant', # Optional
+        # 'context_instruct': 'context_instruct', # Optional
+        # 'turn_template': 'turn_template', # Optional
+        'regenerate': False,
+        '_continue': False,
+        'chat_instruct_command': 'Continue the chat dialogue below. Write a single reply for the character "<|character|>".\n\n<|prompt|>',
+
+        # Generation params. If 'preset' is set to different than 'None', the values
+        # in presets/preset-name.yaml are used instead of the individual numbers.
+        'preset': 'None',
         'do_sample': True,
         'temperature': 0.7,
         'top_p': 0.9,
@@ -28,7 +46,7 @@ def run(prompt):
         'eta_cutoff': 0,  # In units of 1e-4
         'tfs': 1,
         'top_a': 0,
-        'repetition_penalty': 1.15,
+        'repetition_penalty': 1.18,
         'presence_penalty': 0,
         'frequency_penalty': 0,
         'repetition_penalty_range': 0,
@@ -37,7 +55,7 @@ def run(prompt):
         'no_repeat_ngram_size': 0,
         'num_beams': 1,
         'penalty_alpha': 0,
-        'length_penalty': 0.2,
+        'length_penalty': 1,
         'early_stopping': False,
         'mirostat_mode': 0,
         'mirostat_tau': 5,
@@ -58,4 +76,5 @@ def run(prompt):
     response = requests.post(URI, json=request)
 
     if response.status_code == 200:
-        return response.json()['results'][0]['text']
+        result = response.json()['results'][0]['history']["visible"][0][1]
+        return result
